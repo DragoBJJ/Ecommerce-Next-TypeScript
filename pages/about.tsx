d4f454de496a287e812160370c7f8a40a8075b3d
Jakub Pawelski <j.pawelski@interia.pl>
2022-05-24 20:20:37 +0200

    delete unsued file

--- pages/about.tsx
+++
@@ -1,9 +0,0 @@
-const About = () => {
-  return (
-    <div className="flex flex-grow w-screen h-screen justify-center p-2">
-      <h1 className="text-center">Zaiste Programuj</h1>
-    </div>
-  );
-};
-
-export default About;
